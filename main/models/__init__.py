38000cb2d2848bcbeeb67efb2583bbf2e041c481
Kushagra Pandey <pandeyk1@ava-m4.ics.uci.edu>
2023-03-28 13:33:24 -0700

    Removed ADM clf module

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -1,6 +1,5 @@
 from .score_fn.song_sde.ncsnpp import NCSNpp
 from .score_fn.song_sde.ncsnpp_clf import NCSNppClassifier
-from .clf.clf import EncoderUNetModel
 from .sde.vpsde import VPSDE
 from .sde.es3_sde import ES3SDE
 from .wrapper import SDEWrapper
